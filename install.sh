e696db00239f31b438f8ceafb7c734e332774374
David Rigie <daverigie@localhost.localdomain>
2018-01-11 08:11:11 -0500

    update

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ sudo wget http://www.antlr.org/download/antlr-4.7.1-complete.jar
 sudo mv $THISDIR/antlr-4.7.1-complete.jar /usr/local/lib
 sudo cp $THISDIR/antlr4 /usr/bin/
 sudo cp $THISDIR/grun /usr/bin/
+
+echo 'export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"' >> ~/.profile
