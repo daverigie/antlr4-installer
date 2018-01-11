6a123022e67f3268ff7128104ef80e247009437c
David Rigie <daverigie@localhost.localdomain>
2018-01-10 23:05:43 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install antlr-4.7.1 on Linux
+THISDIR=`dirname $0`
+echo $THISDIR
+sudo wget http://www.antlr.org/download/antlr-4.7.1-complete.jar
+sudo mv $THISDIR/antlr-4.7.1-complete.jar /usr/local/lib
+sudo cp $THISDIR/antlr4 /usr/bin/
+sudo cp $THISDIR/grun /usr/bin/
